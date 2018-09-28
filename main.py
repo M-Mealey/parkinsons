9a618e5bc640335edc0c804c58b8f2be63e56b5e
M-Mealey <mealeymn@gmail.com>
2018-09-28 11:45:02 -0500

    added to data loader

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 from nqDataLoader import NqDataLoader
 import sys, os, re, datetime
+from person import Person
 
 fileInTest = "MIT-CS1PD/data_MIT-CS1PD/1401114972.068_001_014.csv"
-dir1 = "MIT-CS1PD"
-dir2 = "MIT-CS2PD"
+dir1 = "MIT-CS1PD/data_MIT-CS1PD/"
+dir2 = "MIT-CS2PD/data_MIT-CS2PD/"
 
 h1 = "MIT-CS1PD/GT_DataPD_MIT-CS1PD.csv"
 h2 = "MIT-CS2PD/GT_DataPD_MIT-CS2PD.csv"
@@ -13,6 +14,14 @@ data = NqDataLoader()
 
 info1 = np.genfromtxt(h1, dtype=None, delimiter=",", skip_header=1)
 print info1[1][1]
+
+group1=[]
+for i in range(0,info1.shape[0]):
+    group1.append(Person(info1[i],dir1))
+
+
+
+
 
 data.loadDataFile(fileInTest)
 badElems = data.sanityCheck()
--- person.py
+++ person.py
@@ -1,3 +1,7 @@
+import numpy as np
+from nqDataLoader import NqDataLoader
+
+
 class Person:
     pID=0
     gt=0
@@ -6,10 +10,49 @@ class Person:
     sTap=0
     nqScore=0
     typingSpeed=0
-    file_1
-    file_2
+    file_1=""
+    file_2=""
+    # list of vectors that represent the results from each test
+    r1 = []
+    r2 = []
 
-    def __init__(self, pInfo): 
+    def __init__(self, pInfo, prefix): 
         # creates a person object
         # takes pInfo (patient info) as argument. This is a vector with the MIT patient information that is in their file. Should be in same order.
+        # prefix = name of folder that holds patient files
+        # should be a 1 row np array, formatted as follows:
+        # array as follows:
+        # col 0    col 1    col 2    col 3    col 4    col 5    col 6        col 7    col 8
+        # pID      gt       updrs108 afTap    sTap     nqScore  typingSpeed  file_1   file_2
+        pID=pInfo[0]
+        gt=pInfo[1]
+        updrs108=pInfo[2]
+        afTap=pInfo[3]
+        sTap=pInfo[4]
+        nqScore=pInfo[5]
+        typingSpeed=pInfo[6]
+        file_1 = prefix + pInfo[7]
+        file_2 = prefix + pInfo[8]
+
+        r1 = self.analyzeData(file_1)
+        r2 = self.analyzeData(file_2)
+
+
+
+
+    def analyzeData(self, file):
+        data = NqDataLoader()
+        data.loadDataFile(file)
+        window = []
+        w_num = 0
+        i=0
+        while data.dataTimeEnd[i]< w_num*90:
+            window.append( [data.dataKeys[i], data.dataHT[i], data.dataFT[i], data.dataTimeStart[i], data.dataTimeEnd[i] ])
+            i=i+1
+        print window
+
+
+
+
+
 
